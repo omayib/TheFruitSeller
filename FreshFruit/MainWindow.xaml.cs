491fbaa5f41095712deffcd287322e37304cdc90
omayib <omayib@gmail.com>
2019-10-08 08:08:23 +0700

    initial commit

--- FreshFruit/MainWindow.xaml.cs
+++ FreshFruit/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using FreshFruit.controller;
+using FreshFruit.model;
+using FruitSeller;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,14 +18,57 @@ using System.Windows.Shapes;
 
 namespace FreshFruit
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, BucketEventListener
     {
+        
+        Seller toni;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            Bucket keranjangBuah = new Bucket(2);
+            BucketController bucketController = new BucketController(keranjangBuah, this);
+
+            toni = new Seller("toni", bucketController);
+
+            ListBoxBucket.ItemsSource = keranjangBuah.findAll();
+        }
+
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            Fruit anggur = new Fruit("Anggur");
+            toni.addFruit(anggur);
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            Fruit Apel = new Fruit("Apel");
+            toni.addFruit(Apel);
+
+        }
+        private void Button3_Click(object sender, RoutedEventArgs e)
+        {
+            Fruit Pisang = new Fruit("Pisang");
+            toni.addFruit(Pisang);
+
+        }
+        private void Button4_Click(object sender, RoutedEventArgs e)
+        {
+            Fruit jeruk = new Fruit("Jeruk");
+            toni.addFruit(jeruk);
+
+        }
+
+        public void onFailed(string message)
+        {
+            MessageBox.Show(message,"Warning");
+        }
+
+        public void onSucceed(string message)
+        {
+            ListBoxBucket.Items.Refresh();
         }
     }
 }
